0cc9a708524b641e53d6faa0833ed494ff8f2c74
Aldo Pramilhiawan <aldopramilhiawan0@gmail.com>
2022-03-02 14:22:01 +0700

    menambahkan komponen

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MyButton from "./Component/MyButton";
+import Navbar from "./Component/Navbar";
+import Footer from "./Component/Footer";
 
-function App() {
+const App = () => {
+  const navHeading = "NAvigation Bar";
+  const navText = "Sosmed";
+
+  const clicked = () => {
+    return alert("Button di pencet")
+  };
+
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar navTest={navText} navHeading={navHeading} /> 
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>hallo... Mr.Aldo</p>
+        <MyButton clicked={clicked}/>
+        <Footer />
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
